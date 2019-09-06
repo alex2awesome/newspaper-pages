ab15f6568629093c64bb1cf258f44c47b9791547
alex2awesome <alex2awesome@gmail.com>
2019-09-06 12:19:38 -0700

    updatyed

--- notebooks/text_nn/grid_search.sh
+++ notebooks/text_nn/grid_search.sh
@@ -3,6 +3,7 @@ for cont_lam in 0 .0001 .001
 do
     for sel_lam in .001 .0025 .005 .0075 .01 .025 .05 .075 .1
     do
-        sbatch runner.sh --export=cont_lam=${cont_lam},sel_lam=${sel_lam}
+        cat  runner.sh.template | sed -e "s/sellam/${sel_lam}/g" -e "s/contlam/${cont_lam}/g" > runner.sh
+        sbatch runner.sh
     done
 done
