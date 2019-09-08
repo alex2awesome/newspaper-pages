2a3fae6793f146ff31e8cc6d1ec2acd0015a12ba
alex2awesome <alex2awesome@gmail.com>
2019-09-08 13:44:54 -0700

    updated

--- notebooks/text_nn/grid_search.sh
+++ notebooks/text_nn/grid_search.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-for cont_lam in 0 .0001 .001
+for sel_lam in 0 .0001 .001
 do
-    for sel_lam in .001 .0025 .005 .0075 .01 .025 .05 .075 .1
+    for cont_lam in .001 .0025 .005 .0075 .01 .025 .05 .075 .1
     do
         cat  runner.sh.template | sed -e "s/sellam/${sel_lam}/g" -e "s/contlam/${cont_lam}/g" > runner.sh
         sbatch runner.sh
