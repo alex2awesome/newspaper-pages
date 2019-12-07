96e30545a4ab1a1bdf096327543b64e0db999d55
alex2awesome <alex2awesome@gmail.com>
2019-12-06 21:35:46 -0800

    updated

---
+++ models/attentionviz/group_attention_script_roberta.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+from tqdm import tqdm
+import json
+import pandas as pd 
+
+bert_data = pd.read_csv('../data/bert-data/train.tsv', sep='\t', nrows=50000)
+attention = {
+    0: {i: defaultdict(list) for i in range(12)},
+    1: {i: defaultdict(list) for i in range(12)}
+}
+
+with open('attention-output-roberta.txt', encoding='utf-8') as f:
+    sent_idx = 0
+    for line in tqdm(f, total=462549168):
+        line = line.strip()
+        if line != '':
+            word, attn = line.split('\t')
+            attn = float(attn)
+            attention[label][layer][word].append(attn)
+        else:
+            sent_idx += 1
+            label_idx = int(sent_idx / (12 * 12))
+            label = bert_data.loc[label_idx, 'label']       
+            layer = int(sent_idx / 12)  % 12
+
+json.dump(attention, open('roberta-attention-grouped.json'))
